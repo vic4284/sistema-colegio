fad89b50f55238e8a780265cb645b7e379e280f2
vic4284 <aa2020721@est.univalle.edu>
2026-06-13 15:43:42 -0400

    pruebas ACTUAL

--- app/Views/Usuarios/index.php
+++ app/Views/Usuarios/index.php
@@ -29,7 +29,7 @@
                     <th>Estado</th>
                     <th>Último inicio de sesión</th>
                     <th>Fecha de creación</th>
-                    <th>Acciones</th>
+                    <th>Acciones</th> 
                 </tr>
             </thead>
             <tbody>
