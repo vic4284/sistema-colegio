296cd56ba471627abda9d1ee2d71601e4847e368
vic4284 <aa2020721@est.univalle.edu>
2026-06-13 14:07:42 -0400

    pruebas

--- app/Views/Login/index.php
+++ app/Views/Login/index.php
@@ -96,7 +96,7 @@
         </div>
 
         <div class="grupo">
-            <label for="contrasena">Contraseñasss</label>
+            <label for="contrasena">Contraseñassss</label>
             <input type="password"
             name="contrasena"
             id="contrasena"
