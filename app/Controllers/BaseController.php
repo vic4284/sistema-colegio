b6397de42f7573e7881511e204b828187a8b5627
vic4284 <aa2020721@est.univalle.edu>
2026-07-01 19:32:46 -0400

    Aplicando modificaciones

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -30,16 +30,13 @@ abstract class BaseController extends Controller
     /**
      * @return void
      */
-    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+   public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
     {
-        // Load here all helpers you want to be available in your controllers that extend BaseController.
-        // Caution: Do not put the this below the parent::initController() call below.
-        // $this->helpers = ['form', 'url'];
-
-        // Caution: Do not edit this line.
         parent::initController($request, $response, $logger);
 
-        // Preload any models, libraries, etc, here.
-        // $this->session = service('session');
+        date_default_timezone_set('America/La_Paz');
+
+        $db = \Config\Database::connect();
+        $db->query("SET time_zone = '-04:00'");
     }
 }
